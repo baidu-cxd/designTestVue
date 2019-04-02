8c5a29a8398ab9e3f1e8279978ac90a87862e3fb
dingzu <349923359@qq.com>
2019-04-02 23:49:12 +0800

    莫态

--- src/components/zhijian/pageDetail.vue
+++ src/components/zhijian/pageDetail.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="page-detail" :class="[this.hasSidebar ? 'has-side-bar': 'not-has-side-bar']">
+        <!-- 页面标题 -->
         <div class="page-header">
             <transition name="fade-page-header">
                 <div :key="this.nowPath" class="header-content">
@@ -22,22 +23,147 @@
                       <p>一个终端设备</p>
                     </div>
                   </div>
+                  <!-- 第三个页面的标题 -->
+                  <div v-if="this.nowPath === 'path-zhijianyun-user'">
+                    <div class="inner-content">
+                      <img src="@/assets/zhijian/shili.svg" alt="" class="icon">
+                      <p>用户管理</p>
+                    </div>
+                  </div>
                 </div>
             </transition>  
         </div>
+        <!-- 页面容器 -->
+        <transition name="fade-page-body">
+          <div class="page-body" :key="this.nowPath">
+
+           <!-- 第一个页面的内容 --> 
+           <div v-if="this.nowPath === 'path-zhijianyun-main'" class="page-body-1">
+               <div class="tab">
+                   <div class="tab-ite">API
+                       <div class="under-line"></div>
+                   </div>
+                   <div class="tab-ite">SDK</div>
+                   <div class="tab-line"></div>
+               </div>
+               <h3>基本信息</h3>
+               <p>包大小：23.28m</p>
+               <p>兼容性：4.0+</p>
+               <div class="block-line"></div>
+               <h3>模型下发</h3>
+               <div class="buttom" @click="toggleModule()">安装到新设备</div>
+               <table>
+                   <tr>
+                       <th>设备名称</th>
+                       <th>模型版本</th>
+                       <th>更新时间</th>
+                   </tr>
+                   <tr>
+                       <td>设备名称</td>
+                       <td>模型版本</td>
+                       <td>更新时间</td>
+                   </tr>
+                   <tr>
+                       <td>设备名称</td>
+                       <td>模型版本</td>
+                       <td>更新时间</td>
+                   </tr>
+                   <tr>
+                       <td>设备名称</td>
+                       <td>模型版本</td>
+                       <td>更新时间</td>
+                   </tr>
+               </table>
+           </div>
+
+           <!-- 第二个页面的内容 --> 
+           <div v-if="this.nowPath === 'path-zhijianyun-set'" class="page-body-2">
+               <h3>模型信息</h3>
+               <div class="card-wrp">
+                   <div class="card" v-for="(card, i) in cards" :key="card.key"
+                     :style ='resolveStyle(i)'>
+                     <div class="card-header">
+                         <img src="@/assets/zhijian/shebeib.svg" alt="">
+                         <h4>{{card.name}}</h4>
+                         <div class="line"></div>
+                         <p>{{card.intro}}</p>
+                         <div class="card-bottom">
+                             <p>状态</p>
+                             <p class="blue">正常</p>
+                         </div>
+                     </div>
+                   </div>
+               </div>
+           </div>
+          </div>
+        </transition>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            cards: [
+                {
+                    key : '1',
+                    name : '模型1',
+                    intro : '简单的介绍',
+                },
+                {
+                    key : '2',
+                    name : '模型1',
+                    intro : '简单的介绍',
+                },
+                {
+                    key : '3',
+                    name : '模型1',
+                    intro : '简单的介绍',
+                },
+                {
+                    key : '4',
+                    name : '模型1',
+                    intro : '简单的介绍',
+                },
+                {
+                    key : '5',
+                    name : '模型1',
+                    intro : '简单的介绍',
+                },
+                {
+                    key : '6',
+                    name : '模型1',
+                    intro : '简单的介绍',
+                },
+                {
+                    key : '7',
+                    name : '模型1',
+                    intro : '简单的介绍',
+                },
+                {
+                    key : '8',
+                    name : '模型1',
+                    intro : '简单的介绍',
+                },
+            ]
+        }
+    },
     props: ['hasSidebar', 'nowPath'],
+    methods: {
+        resolveStyle(i) {
+            return 'animation-delay:' + 50 * ( 1 + i ) + 'ms'
+        },
+        toggleModule() {
+            this.$store.state.module = !this.$store.state.module
+        }
+    }
 }
 </script>
 
 <style lang="stylus">
 .page-detail
   background-color #fff
-  min-height 700px
+  min-height 500px
   box-shadow 0 2px 10px rgba( 0, 0, 0, .1)
   margin 94px 24px 24px 24px 
   transition .4s all ease-in-out
@@ -93,7 +219,166 @@ export default {
 
 
 
+// 页面内容样式
+
+.page-body
+  padding 24px
+  .tab
+    height 48px
+    position relative
+    margin 0 0 8px 0
+    div.tab-ite
+      margin 0 24px 16px
+      font-size 16px
+      line-height 30px
+      height 30px
+      color #000
+      float left
+      position relative
+      transition .2s all ease-in-out
+      &:hover
+        color #2e36e6
+        cursor pointer
+      .under-line
+        position absolute
+        left 0
+        bottom -17px
+        height 2px
+        width 100%
+        background-color #2E36E6
+    .tab-line
+      position absolute
+      left 24px
+      bottom 0
+      right 24px
+      height 1px
+      background-color #ebebeb
+  h3
+    font-size 16px
+    line-height 30px
+    margin 24px 24px 12px
+    color #000
+    font-weight 400
+  p
+    font-size 14px
+    line-height 26px
+    margin 12px 24px
+    color #000
+    opacity .4
+  .block-line
+    margin 24px
+    height 1px
+    border-top 1px dotted #000
+    opacity .3
+  .buttom
+    min-width 80px
+    height 40px
+    line-height 40px
+    background-color #2E36E6
+    color #ffffff
+    font-size 14px
+    transition .2s all ease-in-out
+    margin  12px 24px 12px
+    padding 0 24px  
+    text-align center 
+    display inline-block
+    &:hover
+      background-color #000
+      cursor pointer
+  table
+    margin 24px
+    font-size 13px
+    color rgba(0,0,0,.4)
+    border-collapse collapse
+    width 100%
+    line-height 30px
+    tr
+      border-top 1px solid #ebebeb
+      border-bottom 1px solid #ebebeb
+      text-align left
+    th
+      font-size 14px
+      color rgba(0,0,0,1)
+      text-align left
+      padding 4px
+    td
+      padding 4px
+  .card-wrp
+    overflow hidden
+    .card
+      margin 24px
+      float left 
+      width 210px
+      height 218px
+      background-color #f1f1f1
+      opacity 0
+      animation card-in .4s ease-in-out forwards
+      position relative
+      transition .2s all ease-in-out
+      &:hover
+        cursor pointer
+        background-color #fff
+        box-shadow 0 2px 10px rgba(0,0,0,.1)
+      .card-bottom
+        position absolute
+        height 26px
+        bottom 24px
+        p
+          opacity 1
+          float left
+          display block
+        p.blue
+          color #2E36E6
+          margin-left 24px
+          position relative
+          &:before 
+            content ''
+            display block
+            background-color #2e36e6
+            width 6px
+            height 6px
+            border-radius 100px
+            position absolute
+            top 50%
+            left -8px
+            transform translate(-50%,-50%)
+
+
+      .card-header
+        margin 24px 16px 16px
+        overflow hidden
+        img 
+          float left 
+          width 24px
+        h4
+          font-weight 400
+          font-size 16px
+          color #000
+          line-height 24px
+          float left
+          display block
+          height 24px
+          margin 0 0 0 12px
+      .line
+        margin 16px 0 0 0 
+        background-color #000
+        opacity .1
+        height 1px
+        float left
+        width  178px
+      p
+        float left
+        margin 12px 0
+
 // 动画
+
+@keyframes card-in 
+  0%
+    opacity 0
+    transform translateY(40px)
+  100%
+    opacity 1
+    transform translateY(0px)
 
 .fade-page-header-enter-active
   transition .3s all ease-in-out .4s 
@@ -128,6 +413,18 @@ export default {
   .back
     opacity 0
     transform translateX(-100%)
+
+
+.fade-page-body-enter-active
+  transition .4s all ease-in-out .4s 
+.fade-page-body-leave-active
+  transition .4s all ease-in-out 0s 
+.fade-page-body-enter-to, .fade-page-body-leave
+  transform translateY(0)
+  opacity 1
+.fade-page-body-enter, .fade-page-body-leave-to
+  transform translateY(20px)
+  opacity 0
 </style>
 
 
--- src/views/Zhijian.vue
+++ src/views/Zhijian.vue
@@ -43,10 +43,97 @@
             </nav>
             <p>© 2019 Baidu 使用百度前必读    增值电信业务经营许可证：B1.B2-20100266    京ICP证030173号    隐私政策</p>
         </div>
+
+        <!-- 弹窗 -->
+
+        <transition name="fade-module">
+            <div class="module" @click="toggleModule()" v-if="this.$store.state.module">
+                <div class="module-body-content">
+                  <div class="module-body">
+                      <div class="module-body-head">设备管理</div>
+                      <p>设备参数</p>
+                      <p class="week">包大小：23.28m</p>
+                      <p class="week">兼容性：4.0+</p>
+                      <div class="buttom">立即安装</div>
+                  </div>
+                </div>
+                <div class="module-bg"></div>
+            </div>
+        </transition>
+        
+
     </div>
 </template>
 
 <style lang="stylus">
+
+// 弹窗
+
+.zhi-jian .module
+  position fixed
+  top 0
+  left 0
+  right 0
+  bottom 0
+  z-index 1000
+  .module-body-content
+    width 100%
+    height 100%
+    z-index 2
+    position relative
+  .module-body
+    width 640px
+    min-height 340px
+    background-color #ffffff
+    box-shadow 0 2px 10px rgba(0,0,0,.1)
+    position absolute
+    left 50%
+    top 50%
+    transform translate(-50%,-50%)
+    z-index 2
+    .module-body-head
+      width 100%
+      height 80px
+      background-color #2E36E6
+      font-size 16px
+      color #fff
+      line-height 80px
+      text-align center
+      margin-bottom 24px
+    p
+      font-size 14px
+      color #000
+      opacity 1
+      line-height 26px
+      margin 12px 48px
+      &.week
+        opacity .4
+    .buttom
+      min-width 80px
+      height 40px
+      line-height 40px
+      background-color #2E36E6
+      color #ffffff
+      font-size 14px
+      transition .2s all ease-in-out
+      margin  48px auto
+      padding 0 24px  
+      text-align center 
+      width 140px
+      &:hover
+        background-color #000
+        cursor pointer
+  .module-bg
+    z-index 1
+    background-color rgba(0,0,0,.4)
+    width 100%
+    height 100%
+    position absolute
+    top 0
+    left 0
+
+
+
 body
   background-color #DEDEDE
 .zhi-jian
@@ -98,6 +185,26 @@ body
     text-align center
 
 // 动画
+
+.fade-module-enter-active, .fade-module-leave-active
+  transition .4s all ease-in-out
+  .module-bg
+    transition .4s all ease-in-out
+  .module-body-content
+    transition .2s all ease-in-out
+.fade-module-enter-to, .fade-module-leave
+  .module-bg
+    opacity 1
+  .module-body-content
+    transform translateY(0)
+    opacity 1
+.fade-module-enter, .fade-module-leave-to
+  .module-bg
+    opacity 0
+  .module-body-content
+    transform translateY(40px)
+    opacity 0
+
 .fade-right-enter-active, .fade-right-leave-active
   transition .6s all ease-in-out
 .fade-right-enter-to, .fade-right-leave
@@ -183,6 +290,9 @@ export default {
           } else {
               return false
           }
+      },
+      toggleModule() {
+          this.$store.state.module = !this.$store.state.module
       }
     }
 }
